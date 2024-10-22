299534411356f53a10d6879d9b6b539b2997d902
Denise <denisealiciafranz@gmail.com>
2024-10-22 11:46:16 +0200

    hello

--- ballerburg/ballerburg.js
+++ ballerburg/ballerburg.js
@@ -1,1 +1,17 @@
 "use strict";
+console.log("Hello There");
+window.addEventListener("load", handleLoad);
+function handleLoad(_event) {
+}
+function drawTerrain() {
+}
+function drawCannons() {
+}
+function drawControls() {
+}
+function drawPlayerWindows() {
+}
+function drawBalls() {
+}
+function animationFrame() {
+}
---
+++ ballerburg/ballerburg.ts
@@ -0,0 +1,48 @@
+console.log("Hello There");
+
+window.addEventListener("load", handleLoad);
+
+interface Cannon {
+
+
+}
+
+interface Ball {
+
+
+}
+
+interface PlayerWindow {
+
+
+}
+
+
+function handleLoad(_event: Event): void {
+
+}
+
+function drawTerrain(): void {
+
+}
+
+function drawCannons(): void {
+
+}
+
+function drawControls(): void {
+
+}
+
+function drawPlayerWindows(): void {
+
+}
+
+function drawBalls(): void {
+
+}
+
+
+function animationFrame(): void {
+    
+}
