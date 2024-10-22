e098adce57c67c24d5359392e683f0a3d1848a6a
Denise <denisealiciafranz@gmail.com>
2024-10-22 12:17:58 +0200

    hello :)

--- ballerburg/ballerburg.js
+++ ballerburg/ballerburg.js
@@ -1,7 +1,14 @@
 "use strict";
 console.log("Hello There");
 window.addEventListener("load", handleLoad);
+setInterval(animationFrame, 16);
 function handleLoad(_event) {
+}
+function generateTerrain() {
+}
+function generateCannons() {
+}
+function generateBalls(_cannon) {
 }
 function drawTerrain() {
 }
@@ -9,9 +16,13 @@ function drawCannons() {
 }
 function drawControls() {
 }
-function drawPlayerWindows() {
+function drawPlayerWindow(_window, _cannon) {
 }
-function drawBalls() {
+function drawBalls(_cannon, _ball) {
+}
+function ballFly(_ball) {
+}
+function collsionCheck(_ball) {
 }
 function animationFrame() {
 }
--- ballerburg/ballerburg.ts
+++ ballerburg/ballerburg.ts
@@ -1,24 +1,41 @@
 console.log("Hello There");
 
 window.addEventListener("load", handleLoad);
+setInterval(animationFrame, 16);
 
 interface Cannon {
 
-
+    player: number,
+    angle: number,
+    gunpowder: number,
+    posX: number,
+    posY: number,
 }
 
 interface Ball {
 
-
-}
-
-interface PlayerWindow {
-
+    player: number,
+    speed: number,
+    posX: number,
+    posY: number,
+    inAir: boolean,
 
 }
 
 
 function handleLoad(_event: Event): void {
+
+}
+
+function generateTerrain(): void {
+
+}
+
+function generateCannons(): void {
+
+}
+
+function generateBalls(_cannon: Cannon): void {
 
 }
 
@@ -34,14 +51,21 @@ function drawControls(): void {
 
 }
 
-function drawPlayerWindows(): void {
+function drawPlayerWindow(_window: PlayerWindow, _cannon: Cannon): void {
 
 }
 
-function drawBalls(): void {
+function drawBalls(_cannon: Cannon, _ball: Ball): void {
 
 }
 
+function ballFly(_ball: Ball): void {
+    
+}
+
+function collsionCheck(_ball: Ball): void {
+
+}
 
 function animationFrame(): void {
     
