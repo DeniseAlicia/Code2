f2b10d5aa7a3d78bc95dedadd6ab4c902ffda0ee
Flo <enirolf254@gmail.com>
2024-10-22 15:05:38 +0200

    Update ballerburg.js

--- ballerburg/ballerburg.js
+++ ballerburg/ballerburg.js
@@ -1,9 +1,10 @@
 "use strict";
 console.log("Hello There");
-
-const canvas = document.querySelector("canvas");
+const gravity = 5;
+const cannons = [];
+const cannonBalls = [];
+const canvas = document.getElementsByTagName("canvas")[0];
 const ctx = canvas.getContext("2d");
-let cannons = [];
 window.addEventListener("load", handleLoad);
 setInterval(animationFrame, 16);
 let slider = document.getElementById("angle1");
@@ -12,23 +13,11 @@ slider.oninput = function () {
     Angle1 = Number(slider.value);
     console.log(Angle1);
 };
-function handleLoad(_event) {
-    generateTerrain();
-    generateCannons(2);
-
-const gravity = 5;
-const cannons = [];
-const cannonBalls = [];
-const canvas = document.getElementsByTagName("canvas")[0];
-const ctx = canvas.getContext("2d");
-window.addEventListener("load", handleLoad);
-setInterval(animationFrame, 16);
 function handleLoad() {
     generateTerrain();
     generateCannons(2);
     drawTerrain();
     drawCannons();
-
 }
 function generateTerrain() {
 }
