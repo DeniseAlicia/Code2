4dcf05cb5380f94ea4378cab2f6588d240f351a8
Denise <denisealiciafranz@gmail.com>
2024-10-24 17:26:14 +0200

    myFarm

--- invertedClassroom/oldMacDonald/myFarm/farm.js
+++ invertedClassroom/oldMacDonald/myFarm/farm.js
@@ -2,15 +2,43 @@
 var MyFarm;
 (function (MyFarm) {
     class Animal {
-        constructor() {
+        constructor(_species, _name, _noise, _food, _stomach) {
+            this.species = _species;
+            this.name = _name;
+            this.noise = _noise;
+            this.food = _food;
+            this.stomach = _stomach;
+        }
+        eat() {
+            this.food.stock -= this.stomach;
+            this.food.report();
+        }
+        sing() {
+            console.log(this.name);
+            console.log(this.noise + " " + this.name + "!");
         }
     }
     class FoodSupply {
-        constructor() {
+        constructor(_type, _stock) {
+            this.type = _type;
+            this.stock = _stock;
+        }
+        report() {
+            console.log("There are " + String(this.stock) + "units of " + this.type + " left");
+        }
+        restock() {
+            if (this.stock < 11) {
+                this.stock = 100;
+                console.log(this.type + " has been restocked");
+            }
         }
     }
     class Farm {
-        constructor() {
+        constructor(_stable, _pantry) {
+            this.stable = _stable;
+            this.pantry = _pantry;
+        }
+        simulateDay() {
         }
     }
 })(MyFarm || (MyFarm = {}));
--- invertedClassroom/oldMacDonald/myFarm/farm.ts
+++ invertedClassroom/oldMacDonald/myFarm/farm.ts
@@ -1,20 +1,78 @@
 namespace MyFarm {
 
 class Animal {
-    constructor() {
+
+    species: string;
+    name: string;
+    noise: string;
+    food: FoodSupply;
+    stomach: number;
+
+    constructor(_species: string, _name: string, _noise: string, _food: FoodSupply, _stomach: number) {
+
+        this.species = _species;
+        this.name = _name;
+        this.noise = _noise;
+        this.food = _food;
+        this.stomach = _stomach;
+    }
+
+    eat(): void {
+
+        this.food.stock -= this.stomach;
+        this.food.report();
+    }
+
+    sing(): void {
+
+        console.log(this.name);
+        console.log(this.noise + " " + this.name + "!");
 
     }
+
 }
 
 class FoodSupply {
-    constructor() {
 
+    type: string;
+    stock: number;
+
+    constructor(_type: string, _stock: number) {
+
+        this.type = _type;
+        this.stock = _stock;
+
+    }
+    
+    report(): void {
+
+        console.log("There are " + String(this.stock) + "units of " + this.type + " left");
+    }
+
+    restock(): void {
+
+        if(this.stock < 11) {
+            this.stock = 100;
+            console.log(this.type + " has been restocked");
+        }
     }
 }
 
 class Farm {
-    constructor() {
+
+    stable: Animal[];
+    pantry: FoodSupply[];
+
+    constructor(_stable: Animal[], _pantry: FoodSupply[]) {
+
+        this.stable = _stable;
+        this.pantry = _pantry;
+    }
+
+    simulateDay(): void {
 
     }
 }
+
+
 }
