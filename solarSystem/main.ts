cbe76394374632c562df19f950ee185dbc59ea98
Denise <denisealiciafranz@gmail.com>
2024-10-31 17:47:02 +0100

    main program function shells in main and concept diagrams

--- solarSystem/main.ts
+++ solarSystem/main.ts
@@ -1,12 +1,14 @@
 namespace SolarSystem {
 
-    export let crc2: CanvasRenderingContext2D;
+    //setting up the canvas
+    const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
+    export const crc2: CanvasRenderingContext2D = <CanvasRenderingContext2D> canvas.getContext("2d"); 
+
     window.addEventListener("load", handleLoad);
     
     function handleLoad(): void {
-
-        const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
-        crc2 = <CanvasRenderingContext2D> canvas.getContext("2d");
+        //preparing the simulation
+        console.log("loading page");
 
         createCelestialBodies();
         setInterval(update, 25);
@@ -14,14 +16,17 @@ namespace SolarSystem {
     }
     
     function createCelestialBodies(): void {
+        //creating all simulated celestial bodies
         console.log("creating bodies");
     }
 
     function update(): void {
+        //updating and redrawing the simulation
+        console.log("updating");
 
         crc2.fillStyle = "black";
         crc2.fillRect(0, 0, canvas.width, canvas.height);
-        
+
     }
 
 
