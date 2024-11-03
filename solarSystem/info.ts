e04bf21b6e1b94e381f489424ee6f00a258345e4
Denise <denisealiciafranz@gmail.com>
2024-11-03 11:06:38 +0100

    completed info for mercury

--- solarSystem/info.ts
+++ solarSystem/info.ts
@@ -1,36 +1,124 @@
 namespace SolarSystem {
 
-    //info that is displayed when the celestial bodies are clicked
+    //info that is displayed when the celestial bodies are clicked. ` to create multiple lines
+    //basic planet info: 
+    // Diameter: 
+    // Distance from the sun:
+    // Orbital period:
+    // Max surface temperature: 
+    // Min surface temperature: 
+    // Moons: 
+
+    //basic moon info: 
+    //
 
     //sun
     export const sunInfo: string = "Sun";
 
     //mercury
-    export const mercuryInfo: string = "Mercury";
+    export const mercuryInfo: string = `
+        Diameter: 4878 km
+        Distance from the sun: 58 million km
+        Orbital period: 88 earth days
+        Max surface temperature: 427°C / 700K
+        Min surface temperature: -170°C / 100K
+        Moons: 0
+
+
+    `
+    export const mercuryText: string = "As the smallest planet in our solar system, mercury is smaller than some of the other planet's moons. Some even theorize that it used to be the moon of venus. Mercury is named after the roman god of trade, thieves and travel."
+     
+
 
     //venus
-    export const venusInfo: string = "Venus";
+    export const venusInfo: string = `
+        Diameter:
+        Distance from the sun:
+        Orbital period:
+        Max surface temperature:
+        Min surface temperature:
+        Moons:
+
+    `
 
     //earth + moon
-    export const earthInfo: string = "Earth";
+    export const earthInfo: string = `
+        Diameter:
+        Distance from the sun:
+        Orbital period: 1 earth year
+        Max surface temperature:
+        Min surface temperature:
+        Moons:
+
+    `
     export const moonInfo: string = "Moon";
 
-    //mars
-    export const marsInfo: string = "Mars";
+    //mars 
+    export const marsInfo: string = `
+        Diameter:
+        Distance from the sun:
+        Orbital period:
+        Max surface temperature:
+        Min surface temperature:
+        Moons:
+
+    `
 
     //jupiter + moons
-    export const jupiterInfo: string = "Jupiter";
+    export const jupiterInfo: string =`
+        Diameter:
+        Distance from the sun:
+        Orbital period:
+        Max surface temperature:
+        Min surface temperature:
+        Moons:
 
+    `
     //saturn + moons
-    export const saturnInfo: string = "Saturn";
+    export const saturnInfo: string = `
+        Diameter:
+        Distance from the sun:
+        Orbital period:
+        Max surface temperature:
+        Min surface temperature:
+        Moons:
+
+    `
 
     //uranus
-    export const uranusInfo: string = "Uranus";
+    export const uranusInfo: string = `
+        Diameter:
+        Distance from the sun:
+        Orbital period:
+        Max surface temperature:
+        Min surface temperature:
+        Moons:
+
+    `
 
     //neptun
-    export const neptunInfo: string = "Neptun";
+    export const neptunInfo: string = `
+        Diameter:
+        Distance from the sun:
+        Orbital period:
+        Max surface temperature:
+        Min surface temperature:
+        Moons:
+
+    `
 
     //pluto
-    export const plutoInfo: string = "Pluto";
+    export const plutoInfo: string = `
+        Diameter:
+        Distance from the sun:
+        Orbital period:
+        Max surface temperature:
+        Min surface temperature:
+        Moons:
 
+    `
+
+    //sources
+    //https://www.rmg.co.uk/stories/topics/solar-system-data
+    //wikipedia
 }
