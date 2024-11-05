abed0af42c6f2ac4881e3673ed8ee3bcabbb6c4d
Denise <denisealiciafranz@gmail.com>
2024-11-05 15:15:20 +0100

    visible fudge cube

--- FUDGE/FirstFudge/FirstFudge.js
+++ FUDGE/FirstFudge/FirstFudge.js
@@ -5,14 +5,21 @@ var FirstFudge;
     console.log(f);
     window.addEventListener("load", start); //add a event Listener to the window to load the canvas before executing the script 
     function start() {
-        const canvas = document.querySelector("canvas");
-        console.log(canvas);
-        const cube = new f.MeshCube("Cube");
-        console.log(cube);
-        const camera = new f.ComponentCamera();
+        const canvas = document.querySelector("canvas"); //initialize canvas
+        const camera = new f.ComponentCamera(); //create a new camera
         console.log(camera);
-        const viewport = new f.Viewport();
-        viewport.initialize("Viewport", null, camera, canvas);
+        const mesh = new f.MeshCube("Cube"); //create new cube mesh (a mesh is a resource, multiple components can reference it)
+        console.log(mesh);
+        const cmpMesh = new f.ComponentMesh(mesh); //package the mesh into a component that can be attached to a node
+        const material = new f.Material("Material", f.ShaderLit); //create a new material for the cube
+        const cmpMaterial = new f.ComponentMaterial(material); //put new material in a component
+        const node = new f.Node("Node"); //create a new node
+        node.addComponent(cmpMesh); //the component with the mesh gets added to the node
+        node.addComponent(cmpMaterial); //the componen with the material gets added to the node
+        camera.mtxPivot.translateZ(-5);
+        const viewport = new f.Viewport(); //create a new viewport to display stuff
+        viewport.initialize("Viewport", node, camera, canvas); //giving the viewport a name, a branch of nodes to render, a camera and a canvas to draw on
+        viewport.draw(); //actually draw the viewport
     }
 })(FirstFudge || (FirstFudge = {}));
 //# sourceMappingURL=FirstFudge.js.map
--- FUDGE/FirstFudge/FirstFudge.ts
+++ FUDGE/FirstFudge/FirstFudge.ts
@@ -5,16 +5,26 @@ namespace FirstFudge {
     window.addEventListener("load", start); //add a event Listener to the window to load the canvas before executing the script 
 
     function start(): void {
-        const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
-        console.log(canvas);
-
-        const cube: f.MeshCube = new f.MeshCube("Cube");
-        console.log(cube);
-
-        const camera: f.ComponentCamera = new f.ComponentCamera();
+        const canvas: HTMLCanvasElement = document.querySelector("canvas")!; //initialize canvas
+              
+        const camera: f.ComponentCamera = new f.ComponentCamera(); //create a new camera
         console.log(camera);
 
-        const viewport: f.Viewport = new f.Viewport();
-        viewport.initialize("Viewport", null, camera, canvas);
+        const mesh: f.MeshCube = new f.MeshCube("Cube"); //create new cube mesh (a mesh is a resource, multiple components can reference it)
+        console.log(mesh);
+        const cmpMesh: f.Component = new f.ComponentMesh(mesh); //package the mesh into a component that can be attached to a node
+
+        const material: f.Material = new f.Material("Material", f.ShaderLit); //create a new material for the cube
+        const cmpMaterial: f.ComponentMaterial = new f.ComponentMaterial(material); //put new material in a component
+  
+        const node: f.Node = new f.Node("Node"); //create a new node
+        node.addComponent(cmpMesh);     //the component with the mesh gets added to the node
+        node.addComponent(cmpMaterial); //the componen with the material gets added to the node
+
+        camera.mtxPivot.translateZ(-5);
+
+        const viewport: f.Viewport = new f.Viewport(); //create a new viewport to display stuff
+        viewport.initialize("Viewport", node, camera, canvas); //giving the viewport a name, a branch of nodes to render, a camera and a canvas to draw on
+        viewport.draw(); //actually draw the viewport
     }
 }
