4a9645681376def3e6fed565f6d6cdaa4606d61c
Flo <enirolf254@gmail.com>
2024-11-13 20:53:27 +0100

    test

---
+++ Test/test.js
@@ -0,0 +1,10 @@
+"use strict";
+const cardOne = document.getElementById("one");
+const Position1 = cardOne.getBoundingClientRect();
+console.log(Position1);
+const cardTwo = document.getElementById("two");
+const Position2 = cardTwo.getBoundingClientRect();
+console.log(Position2);
+// cardOne.addEventListener("mouseover", turnCard);
+cardOne.addEventListener("onClick", handleTouch12);
+//# sourceMappingURL=test.js.map
---
+++ Test/test.ts
@@ -0,0 +1,14 @@
+const cardOne = document.getElementById("one")!;
+const Position1 = cardOne.getBoundingClientRect();
+console.log(Position1);
+
+const cardTwo = document.getElementById("two")!;
+const Position2 = cardTwo.getBoundingClientRect();
+console.log(Position2);
+
+// cardOne.addEventListener("mouseover", turnCard);
+
+cardOne.addEventListener("onClick", handleTouch12);
+
+
+
