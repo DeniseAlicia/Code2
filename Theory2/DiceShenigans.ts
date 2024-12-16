358ef870df4098099a061f1fb6ff8d3770073b21
Denise <denisealiciafranz@gmail.com>
2024-12-16 16:38:27 +0100

    ESNext in config

--- Theory2/DiceShenigans.ts
+++ Theory2/DiceShenigans.ts
@@ -2,7 +2,7 @@ export namespace DicePotential {
     console.log("Hello there")
     export function strategy(_score: number[], _active: number, _potential: number): boolean {
         
-        return Math.random() < 0.5;
+        return Math.random() < 0.8;
 
     }
 }
