f65200fc37dd7c2f87c3cb8db57d08d723bc088a
Denise <denisealiciafranz@gmail.com>
2024-12-16 16:43:12 +0100

    new compile

--- Theory2/DiceShenigans.js
+++ Theory2/DiceShenigans.js
@@ -1,12 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DicePotential = void 0;
-var DicePotential;
+export var DicePotential;
 (function (DicePotential) {
     console.log("Hello there");
     function strategy(_score, _active, _potential) {
-        return Math.random() < 0.5;
+        return Math.random() < 0.8;
     }
     DicePotential.strategy = strategy;
-})(DicePotential || (exports.DicePotential = DicePotential = {}));
+})(DicePotential || (DicePotential = {}));
 //# sourceMappingURL=DiceShenigans.js.map
